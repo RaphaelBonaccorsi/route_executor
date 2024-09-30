b2c30499bdebd27e031b4eaf7c0a723cf069bd90
Raphael Bonaccorsi <81281352+RaphaelBonaccorsi@users.noreply.github.com>
2024-09-30 11:59:25 -0300

    Add files via upload

--- route_executor_launch.py
+++ route_executor_launch.py
@@ -1,26 +1,40 @@
 import os
-from launch import LaunchDescription
-from launch.actions import ExecuteProcess, TimerAction
-from launch_ros.actions import Node
+
 from ament_index_python.packages import get_package_share_directory
 
-def generate_launch_description():
-    # Define the correct PX4-Autopilot directory
-    px4_autopilot_dir = '/home/harpia/PX4-Autopilot'  # Replace with the correct absolute path
-    
-    # Get the package share directory for route_executor
-    package_share = get_package_share_directory('route_executor')
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
 
-    return LaunchDescription([
-        Node(
-            package='route_executor',
-            executable='route_executor',
-            output='screen'
-        ),
-        Node(
-            package='pddl_reader',
-            executable='pddl_reader',
-            output='screen'
-        ),
-        
-    ])
+def generate_launch_description():
+    plansys2_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(
+            get_package_share_directory('plansys2_bringup'),
+            'launch',
+            'plansys2_bringup_launch_monolithic.py')),
+        launch_arguments={'model_file': './pddl/domain.pddl'}.items()
+        )
+    
+    route_executor_node = Node(
+        package='route_executor',
+        executable='route_executor',
+        output='screen'
+    )
+
+    pddl_reader_node = Node(
+        package='pddl_reader',
+        executable='pddl_reader',
+        output='screen'
+    )
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(plansys2_cmd)
+    ld.add_action(route_executor_node)
+    ld.add_action(pddl_reader_node)
+ 
+
+    return ld
